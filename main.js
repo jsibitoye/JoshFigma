db9fa9f9721f488c071149db4bdb87c19a27f9f2
J.s Ibitoye <jsibitoye@outlook.com>
2022-07-07 07:56:01 +0100

    Updated

--- main.js
+++ main.js
@@ -1,23 +1,24 @@
-const nav = document.querySelector("nav");
-const mobileNav = document.querySelector("nav.mobile-nav");
-const menuIcon = document.querySelector(".menu-icon");
-const closeIcon = document.querySelector(".mobile-menu-container .close-icon");
-const mobileMenuContainer = document.querySelector(".mobile-menu-container");
+function initializer(){
+  let fan = document.getElementById("fanContainer");
 
-window.addEventListener("scroll", () => {
-  if (window.pageYOffset > 60) {
-    nav.classList.add("scrolled");
-    mobileNav.classList.add("scrolled");
-  } else {
-    nav.classList.remove("scrolled");
-    mobileNav.classList.remove("scrolled");
+  let circle =396, toDegree = 36;
+
+  for (i=1; i<=10; i++){
+    let imgfile = document.createElement("img");
+    imgfile.src = "./img/rectangle3.svg"
+    imgfile.style= "animation: animate"+i+" 30s linear infinite;"
+    circle += -36;
+    toDegree += - 36;
+    const keyFrames = document.createElement("style");
+    keyFrames.innerHTML = `
+      @keyframes animate`+i+ ` { 0%{
+                                  transform:rotateZ(`+circle+`deg);
+                                  transform-origin: right bottom;
+                                } 100%{
+                                  transform: rotateZ( `+toDegree+`deg);
+                                  transform-origin:right bottom;
+                                }  }`;
+    imgfile.appendChild(keyFrames);
+    fan.appendChild(imgfile);
   }
-});
-
-menuIcon.addEventListener("click", () => {
-  mobileMenuContainer.classList.add("active");
-});
-
-closeIcon.addEventListener("click", () => {
-  mobileMenuContainer.classList.remove("active");
-});
+}
